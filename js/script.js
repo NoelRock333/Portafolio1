027b90b274642a618344d1c2ce4c2f0b4ffadfb3
Noel Alejandro <noel_ska15@hotmail.com>
2014-11-11 00:26:48 -0600

    Refactorizado código del menú con scroll mágico

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
 $(document).on('ready', function(){
-	/* ---- Efectos para posicionarse ---- */
-    var lastId;
-    var topMenu = $(".menu");
+
+/* ---- Efectos para posicionarse ---- */
+    // Obtenemos la altura total del menú
     var topMenuHeight = $('.menu').outerHeight(true);
     // Lista de items del menu
-    var menuItems = topMenu.find("a"),
-        // Anchors correspondientes a items del menu
-        scrollItems = menuItems.map(function () {
-            var item = $($(this).attr("href"));
-            if (item.length) {
-                return item;
-            }
-        });
-
-    // vigila evento click de los items del menu
+    var menuItems = $(".menu").find("a");
+    // vigila evento click de los items del menú
     // así obtenemos un chido efecto animado de scroll
-    menuItems.click(function (e) {
+    menuItems.on("click", function (e) {
         e.preventDefault();
-        var href = $(this).attr("href"),
-            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        var href = $(this).attr("href");
+        var offsetTop = 0;
 
+        if(href === "#")
+            offsetTop = 0;
+        else 
+            offsetTop = $(href).offset().top - topMenuHeight + 1;
+        // Primero detiene la animación anterior y luego le aplica la nueva
         $('html, body').stop().animate({
             scrollTop: offsetTop
         }, 400);
     });
+/* ---- FIN Efectos para posicionarse ---- */
 
+
+/* ---- Creador de barras de habilidad ---- */
     $(".habilidad").each(function(){
         var porcentaje = $(this).find(".barra").data("valor");
         $(this).find(".barra").css("width", porcentaje + "%");
     });
+/* ---- FIN Creador de barras de habilidad  ---- */
 
 });
