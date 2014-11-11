eaba6fce9a29617fc5bae204b5cc56213581ccc2
Noel Alejandro <noel_ska15@hotmail.com>
2014-11-10 22:39:00 -0600

    Sacado codigo del posicionador en un archivo diferente

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).on('ready', function(){
+	/* ---- Efectos para posicionarse ---- */
+    var lastId;
+    var topMenu = $(".menu");
+    var topMenuHeight = $('.menu').outerHeight(true);
+    // Lista de items del menu
+    var menuItems = topMenu.find("a"),
+        // Anchors correspondientes a items del menu
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
+
+    // vigila evento click de los items del menu
+    // así obtenemos un chido efecto animado de scroll
+    menuItems.click(function (e) {
+        e.preventDefault();
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 400);
+    });
+
+});
