3173b2717229b05dca8180340cfd3b57d8e101db
Noel Alejandro <noel_ska15@hotmail.com>
2014-11-17 21:05:13 -0600

    Incluida estructura basica de Express

---
+++ public/javascripts/script.js
@@ -0,0 +1,35 @@
+$(document).on('ready', function(){
+
+/* ---- Efectos para posicionarse ---- */
+    // Obtenemos la altura total del menú
+    var topMenuHeight = $('.menu').outerHeight(true);
+    // Lista de items del menu
+    var menuItems = $(".menu").find("a");
+    // vigila evento click de los items del menú
+    // así obtenemos un chido efecto animado de scroll
+    menuItems.on("click", function (e) {
+        e.preventDefault();
+        var href = $(this).attr("href");
+        var offsetTop = 0;
+
+        if(href === "#")
+            offsetTop = 0;
+        else 
+            offsetTop = $(href).offset().top - topMenuHeight + 1;
+        // Primero detiene la animación anterior y luego le aplica la nueva
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 400);
+    });
+/* ---- FIN Efectos para posicionarse ---- */
+
+
+/* ---- Creador de barras de habilidad ---- */
+    $(".habilidad").each(function(){
+        $(this).append('<div class="barra"></div>'); // Agregamos un nuevo elemento HTML
+        var porcentaje = $(this).data("valor"); // Obtenemos el valor de la propiedad 'data-valor'
+        $(this).find(".barra").css("width", porcentaje + "%"); // Asignamos tamaño de la barra con CSS in line
+    });
+/* ---- FIN Creador de barras de habilidad  ---- */
+
+});
